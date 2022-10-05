1ab4c0a27e36bdfd4cd644ee89051310ed6af4a7
Akerele Anu <anuoluwapoakr123@gmail.com>
2022-10-05 07:16:04 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+alert("you are in the wrong site siko ");
+document.write("hello,get out!");
+print("hello welcome to javascript")
+ans=print("hello what is 3+2")
+if ans =5
+print("congrats u are a winner")
